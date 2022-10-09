b1a88fde6a05eaf9a7f7f52be04ef2c7c2d701a3
annastmal <103998851+annastmal@users.noreply.github.com>
2022-10-09 15:13:12 +0100

    filled in the structure in Form folder

---
+++ src/components/Form/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Form() {
+  return (
+    <>
+      <a href="https://github.com/annastmal/WeatherApp" target="blank">
+        {" "}
+        Open-source coded by Anna Malanchuk
+      </a>
+    </>
+  );
+}
+export default Form;
